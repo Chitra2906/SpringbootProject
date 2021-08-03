5a303646ed700664d6c181404e5a95b1181729b4
Chitra2906 <chitradey29@gmail.com>
2021-08-04 01:20:57 +0530

    deleted showuser.jsp replaced ModelAndView to JSONObject response in showUser()

--- bootjpa/src/main/java/com/bootjpa/controller/UserController.java
+++ bootjpa/src/main/java/com/bootjpa/controller/UserController.java
@@ -4,14 +4,19 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.bootjpa.dao.UserRepo;
 import com.bootjpa.model.User;
+import com.fasterxml.jackson.databind.util.JSONPObject;
 
 @Controller
+
 public class UserController {
 	
 	@Autowired
@@ -28,15 +33,27 @@ public class UserController {
 		return "home.jsp";
 	}
 	
+//	@RequestMapping("/showUser")
+//	public ModelAndView showUser(@RequestParam int userid) {
+//		ModelAndView mv = new ModelAndView("showuser.jsp");
+//		User user=repo.findById(userid).orElse(new User());
+//		System.out.println(user);
+//		mv.addObject(user); //model object
+//		System.out.println("user added");
+//		return mv;
+//	}
+	
+	
+	
 	@RequestMapping("/showUser")
-	public ModelAndView showUser(@RequestParam int userid) {
-		ModelAndView mv = new ModelAndView("showuser.jsp");
+	@ResponseBody
+	public   JSONPObject showUser(@RequestParam int userid) {
 		User user=repo.findById(userid).orElse(new User());
-		System.out.println(user);
-		mv.addObject(user); //model object
-		System.out.println("user added");
-		return mv;
+		System.out.println("show user called");
+		return new JSONPObject("userobj", user);
 	}
+	
+	
 	
 	@RequestMapping("/deleteUser")
 	public String deleteUser(@RequestParam int userid) {
